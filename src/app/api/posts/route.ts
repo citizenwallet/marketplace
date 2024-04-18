fc198f00717deb558774fa6db8885234ed6f38f8
Xavier Damman <xdamman@gmail.com>
2024-04-18 17:09:40 +0200

    update .env.example

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,14 +2,12 @@ import { db } from "@/lib/db";
 
 export async function POST(request: Request) {
   const data = await request.json();
-  console.log(">>> server insert", data);
   const res = await db.insert("posts", data);
   return Response.json(res);
 }
 
 export async function PATCH(request: Request) {
   const data = await request.json();
-  console.log(">>> server update", data);
   const res = await db.update("posts", data);
   return Response.json(res);
 }
