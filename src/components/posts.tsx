5943d823c160ed7a1c48870934e59193d13b0a20
Xavier Damman <xdamman@gmail.com>
2025-03-14 18:56:43 +0100

    fix build issue

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -22,7 +22,12 @@ export default async function Posts({
     <div>
       <div className="space-y-2">
         {posts.map((post) => (
-          <PostRow key={post.id} data={post} account={account} lang={lang} />
+          <PostRow
+            key={post.id}
+            data={post}
+            loggedInAccountAddress={account}
+            lang={lang}
+          />
         ))}
       </div>
       <div className="border-t border-gray-200 dark:border-gray-800" />
