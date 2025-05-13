6232ae6e2eaeec2fc09f3883193192151f1143b9
kev-techi <kevin@techi.be>
2025-05-13 10:34:22 +0200

    display button to handle clicking on contact details

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -15,64 +15,47 @@ type ContactData = {
 
 export default async function Profile({ data }: { data: ContactData }) {
   function className(service: string) {
-    if (["phone", "email"].includes(service)) return "dark:invert";
-    return "";
+    return "brightness-0 invert";
   }
-  // function getLink(service: string, address: string) {
-  //   switch (service) {
-  //     case "whatsapp":
-  //       return `https://wa.me/${data.contactAddress?.replace(
-  //         /^00|\+|\s|\./g,
-  //         ""
-  //       )}?text=${encodeURIComponent(data.title || "")}`;
-  //     case "telegram":
-  //       return `https://t.me/${data.contactAddress?.replace(/@/, "")}`;
-  //     case "email":
-  //       return `mailto:${address}?subject=${encodeURIComponent(
-  //         data.title || ""
-  //       )}`;
-  //     case "phone":
-  //       return `tel:${address}`;
-  //   }
-  //   return "#";
-  // }
+  function getLink(service: string, address: string) {
+    console.log(service, address);
+    switch (service) {
+      case "whatsapp":
+        return `https://wa.me/${data.contactAddress?.replace(
+          /^00|\+|\s|\./g,
+          ""
+        )}?text=${encodeURIComponent(data.title || "")}`;
+      case "telegram":
+        return `https://t.me/${data.contactAddress?.replace(/@/, "")}`;
+      case "email":
+        return `mailto:${address}?subject=${encodeURIComponent(
+          data.title || ""
+        )}`;
+      case "phone":
+        return `tel:${address}`;
+    }
+    return "#";
+  }
 
   return (
-    <div className="my-4">
-      <div className="flex flex-col text-center justify-center w-full my-4">
-        <center>
+    <div className="my-2 py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 cursor-pointer">
+      <a
+        href={getLink(data.contactService ?? "", data.contactAddress ?? "")}
+        target="_blank"
+      >
+        <div className="h-[24px] flex text-center align-center justify-center w-full gap-2">
+          <div className="h-[24px] flex items-center justify-center text-sm text-white">
+            {data.contactAddress}
+          </div>
           <img
-            className={className(data.contactService ?? "")}
+            className={`${className(data.contactService ?? "")}`}
             src={icons[data.contactService ?? ""]}
-            width={64}
-            height={64}
+            width={24}
+            height={24}
             alt={data.contactService ?? ""}
           />
-        </center>
-        <div className="text-sm dark:text-white ">{data.contactAddress}</div>
-      </div>
+        </div>
+      </a>
     </div>
   );
-
-  // return (
-  //   <div className="my-4">
-  //     <a
-  //       href={getLink(data.contactService ?? "", data.contactAddress ?? "")}
-  //       target="_blank"
-  //     >
-  //       <div className="flex flex-col text-center justify-center w-full my-4">
-  //         <center>
-  //           <img
-  //             className={className(data.contactService ?? "")}
-  //             src={icons[data.contactService ?? ""]}
-  //             width={64}
-  //             height={64}
-  //             alt={data.contactService ?? ""}
-  //           />
-  //         </center>
-  //         <div className="text-sm dark:text-white ">{data.contactAddress}</div>
-  //       </div>
-  //     </a>
-  //   </div>
-  // );
 }
